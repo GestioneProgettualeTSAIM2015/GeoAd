6e8101ff64179137b6d61b173bf65d64faae09de
tommytopas <topasdrum@gmail.com>
2015-05-11 21:41:13 +0200

    first commit, test odata with dummy products

---
+++ TestOData/TestOData/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TestOData.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ TestOData/TestOData/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using System.Web.Http.OData;
+using System.Web.Http.OData.Query;
+using System.Web.Http.OData.Routing;
+using Microsoft.Data.OData;
+using TestOData.Models;
+
+namespace TestOData.Controllers
+{
+    public class ProductsController : ODataController
+    {
+        // GET odata/Products
+        [EnableQuery]
+        public IQueryable<Product> GetProducts()
+        {
+            return Repository.Instance.GetProducts().AsQueryable();
+        }
+    }
+}
