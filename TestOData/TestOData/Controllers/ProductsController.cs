64d76c8a7e68a2c9d42b042f428964ffa127ef0d
tommytopas <topasdrum@gmail.com>
2015-05-12 22:50:03 +0200

    test with postgresql and odata

--- TestOData/TestOData/Controllers/ProductsController.cs
+++ TestOData/TestOData/Controllers/ProductsController.cs
@@ -11,6 +11,7 @@ using System.Web.Http.OData.Query;
 using System.Web.Http.OData.Routing;
 using Microsoft.Data.OData;
 using TestOData.Models;
+using Npgsql;
 
 namespace TestOData.Controllers
 {
@@ -20,7 +21,32 @@ namespace TestOData.Controllers
         [EnableQuery]
         public IQueryable<Product> GetProducts()
         {
-            return Repository.Instance.GetProducts().AsQueryable();
+            int id = RequestContext.Principal.Identity.GetHashCode();
+
+            DataSet ds = new DataSet();
+            DataTable dt;
+            string connectionString = "Server=localhost;Port=5012;UserId=postgres;Password=admin;Database=GeoAdDb";
+            NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+            connection.Open();
+            string query = "SELECT * FROM public.\"Products\"";
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(query, connection);
+            ds.Reset();
+            da.Fill(ds);
+            int tables = ds.Tables.Count;
+            dt = ds.Tables[0];
+
+            List<Product> products = new List<Product>(dt.Rows.Count);
+            foreach (DataRow dr in dt.Rows)
+            {
+                products.Add(new Product
+                {
+                    Id = dr.Field<int>("Id"),
+                    CategoryId = dr.Field<int>("CategoryId"),
+                    Name = dr.Field<string>("Name")
+                });
+            }
+
+            return products.AsQueryable();
         }
     }
 }
