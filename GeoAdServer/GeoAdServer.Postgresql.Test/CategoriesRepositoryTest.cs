16f19ce87af81d29b162df5748efc2d16836546c
Tommaso Bertoni <bertonitommaso@gmail.com>
2015-05-13 17:33:08 +0200

    UnitTest for Postgresql.CategoriesRepository

--- GeoAdServer/GeoAdServer.Postgresql.Test/CategoriesRepositoryTest.cs
+++ GeoAdServer/GeoAdServer.Postgresql.Test/CategoriesRepositoryTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using GeoAdServer.Domain.Contracts;
+using System.Collections.Generic;
 
 namespace GeoAdServer.Postgresql.Test
 {
@@ -10,10 +11,19 @@ namespace GeoAdServer.Postgresql.Test
         [TestMethod]
         public void TestMethod1()
         {
+            IList<string> types = new List<string>(new string[]
+            {
+                "ca",
+                "poi"
+            });
+
             string connectionString = "Server=localhost;Port=5012;UserId=postgres;Password=admin;Database=GeoAdDb";
             ICategoriesRepository pgrsRep = new PostgresqlCategoriesRepository(connectionString);
+
             foreach (string cat in pgrsRep.GetAll())
-                Console.WriteLine(cat);
+                Assert.IsTrue(types.Remove(cat.ToLower()));
+
+            Assert.IsTrue(types.Count == 0);
         }
     }
 }
