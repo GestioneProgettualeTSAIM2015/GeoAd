50eb4ef5e56dee8bed579a18432f1128d1a37422
Tommaso Bertoni <bertonitommaso@gmail.com>
2015-05-14 19:08:59 +0200

    first commit webapi project - incomplete

---
+++ GeoAdServer/GeoAdServer.WebApi/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace GeoAdServer.WebApi.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Login()
+        {
+            ViewBag.Title = "Login Page";
+
+            return View();
+        }
+    }
+}
---
+++ GeoAdServer/GeoAdServer.WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.OData;
+
+namespace GeoAdServer.WebApi.Controllers
+{
+    [Authorize]
+    public class ValuesController : ODataController
+    {
+        // GET odata/values
+        [Authorize]
+        [EnableQuery]
+        public IQueryable<string> Get()
+        {
+            return (new string[] { "value1", "value2", RequestContext.Principal.Identity.GetHashCode().ToString()}).AsQueryable();
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
