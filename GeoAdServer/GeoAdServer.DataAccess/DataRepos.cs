d1c2330257e3dbef35fd518bc71527fdc768f5f1
Tommaso Bertoni <bertonitommaso@gmail.com>
2015-05-17 14:46:50 +0200

    data access library

---
+++ GeoAdServer/GeoAdServer.DataAccess/DataRepos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeoAdServer.Domain.Contracts;
+using GeoAdServer.Postgresql;
+
+namespace GeoAdServer.DataAccess
+{
+    public class DataRepos
+    {
+        public static ILocationsRepository Locations { get; private set; }
+
+        public static IPhotosRepository Photos { get; private set; }
+
+        public static IOfferingsRepository Offerings { get; private set; }
+
+        static DataRepos()
+        {
+            string connectionString = "Server=localhost;Port=5012;UserId=postgres;Password=admin;Database=GeoAdDb";
+            Locations = new PostgresqlLocationsRepository(connectionString);
+            var connection = (Locations as PostgresqlLocationsRepository).Connection;
+            Photos = new PostgresqlPhotosRepository(connection);
+            Offerings = new PostgresqlOfferingsRepository(connection);
+        }
+    }
+}
