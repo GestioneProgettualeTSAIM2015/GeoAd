e6b598946f80e92115b632967a8b700e77a6b7dc
Matteo Sist <matteo.sist@aditsolutions>
2015-05-18 10:04:24 +0200

    Add empty eclipse project.

---
+++ GeoAd/src/it/itskennedy/tsaim/MainActivity.java
@@ -0,0 +1,15 @@
+package it.itskennedy.tsaim;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
