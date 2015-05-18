8f081260f9c724305c5835d84519548ad03da1eb
Andrea Cadorin <cadorin89@gmail.com>
2015-05-18 10:24:11 +0200

    Content providers push

---
+++ GeoAd/app/src/main/java/it/itskennedy/tsaim/geoad/localdb/OffersHelper.java
@@ -0,0 +1,22 @@
+package it.itskennedy.tsaim.geoad.localdb;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Cado on 13/05/2015.
+ */
+public class OffersHelper implements BaseColumns {
+
+    public static final String TABLE_NAME = "offers";
+
+    public static final String LOCATION_ID = "locationid";
+    public static final String DESC = "description";
+    public static final String EXP_DATE = "expiredate";
+
+    public final static String CREATE_QUERY = "CREATE TABLE " + TABLE_NAME +"("
+            + _ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + LOCATION_ID + " INTEGER NOT NULL, "
+            + DESC + " TEXT "
+            + EXP_DATE + " DATETIME "
+            + "); ";
+}
