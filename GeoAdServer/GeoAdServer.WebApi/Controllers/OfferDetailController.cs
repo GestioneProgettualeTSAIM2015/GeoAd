851fb163edaebf5acb933dc68661627965b3e2fb
Tommaso Bertoni <bertonitommaso@gmail.com>
2015-06-30 14:00:06 +0200

    store images
    OfferDetailController
    OfferDetail/Show view

---
+++ GeoAdServer/GeoAdServer.WebApi/Controllers/OfferDetailController.cs
@@ -0,0 +1,22 @@
+﻿using GeoAdServer.DataAccess;
+using GeoAdServer.Domain.Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeoAdServer.WebApi.Controllers
+{
+    public class OfferDetailController : Controller
+    {
+        public ActionResult Show(int id)
+        {
+            using (var repos = DataRepos.Instance)
+            {
+                OfferDTO offer = repos.Offers.GetById(id);
+                return View(offer);
+            }
+        }
+    }
+}
